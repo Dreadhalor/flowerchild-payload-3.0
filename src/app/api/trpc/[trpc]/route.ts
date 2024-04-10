606df2489e2fedce98324c66ca2c80f20b6e4b7d
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-10 16:15:55 -0700

    Fixed version incompatibilities Rearranged files Wrapping app in trpc provider

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,6 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-import { appRouter } from '@flowerchild/server';
+import { appRouter } from '@flowerchild/trpc';
 
 const handler = (req: Request) =>
   fetchRequestHandler({
